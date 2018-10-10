62d9f2d472171e370bfc201f0eeb1f2c0a443c38
lewisek657 <cytrynowka657@wp.pl>
2018-10-10 17:29:31 +0200

    Initial commit

---
+++ java/pl/infoshare/App.java
@@ -0,0 +1,20 @@
+package pl.infoshare;
+
+import com.sun.org.apache.xml.internal.security.Init;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Byte name = 25;
+        String city = "Gdynia ccdsdsdsczzzz";
+        System.out.println(name + "\n" +
+                "   " + city);
+        //System.out.println(name);
+        //System.out.println(city);
+    }
+}

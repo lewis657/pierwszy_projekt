e7bbac5219178541c20c5ee2efc75e85a72a98f1
lewisek657 <cytrynowka657@wp.pl>
2018-10-10 20:05:46 +0200

    siema

--- src/main/java/pl/infoshare/App.java
+++ src/main/java/pl/infoshare/App.java
@@ -2,19 +2,50 @@ package pl.infoshare;
 
 import com.sun.org.apache.xml.internal.security.Init;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
         Byte name = 25;
         String city = "Gdynia ccdsdsdsczzzz";
-        System.out.println(name + "\n" +
-                "   " + city);
+        System.out.println(name + "\n" + "   " + city);
         //System.out.println(name);
         //System.out.println(city);
+
+        char wiek = 'a';
+        switch (wiek) {
+            case 'a': {
+                System.out.println("joo");
+
+                break;
+            }
+            default: {
+                System.out.println("nie");
+
+            }
+        }
+short age= 11;
+        System.out.println("siema :"+ (age >12 ? "tak" : "nie") );
+
+        //wczytwanie danych z klawaitury
+
+       /* Sanner scanner = new Scanner(System.in);
+        System.out.print("daj wiek:");
+        String agefk = scanner.nextLine();
+        System.out.println("podaes " +agefk);
+*/
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("podaj liczbe calkowita:");
+        byte aga = scanner.nextByte();
+        if (age != byte)
+        {
+            System.out.println("zla warosc liczby");
+        }
+        double agaa = aga;
+        System.out.println("Liczba zmiennoprzecinkowa " +agaa);
+
     }
 }
